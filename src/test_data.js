8fda1706722b21db2a88292a999576e010f1242c
dhanunjay t <tdhanunjay2@gmail.com>
2020-12-24 13:30:40 +0530

    first

---
+++ src/test_data.js
@@ -0,0 +1,422 @@
+export default
+
+  [
+    {
+
+      "TASK NAME" : "Soledad",
+      "LOB": "Mockus",
+      "SUBTASKS": "Sinclair Machine Products Inc",
+      "ASSIGNED TO": "75 Elm Rd #1190",
+      "STEP": "ACT",
+      "DUE DATE": 2600,
+      
+    },
+    {
+      "TASK NAME": "Dana",
+      "LOB": "Vock",
+      "SUBTASKS": "Fried, Monte Esq",
+      "ASSIGNED TO": "49 Walnut St",
+      "STEP": "ACT",
+      "DUE DATE": 2600,
+     
+    },
+    {
+      "TASK NAME": "Roy",
+      "LOB": "Nybo",
+      "SUBTASKS": "Phoenix Phototype",
+      "ASSIGNED TO": "823 Fishers Ln",
+      "STEP": "ACT",
+      "DUE DATE": 2603,
+      
+    },
+    {
+      "TASK NAME": "Annamae",
+      "LOB": "Lothridge",
+      "SUBTASKS": "Highland Meadows Golf Club",
+      "ASSIGNED TO": "584 Meridian St #997",
+      "STEP": "ACT",
+      "DUE DATE": 2608,
+    
+    },
+    {
+      "TASK NAME": "Katheryn",
+      "LOB": "Lamers",
+      "SUBTASKS": "Sonoco Products Co",
+      "ASSIGNED TO": "62171 E 6th Ave",
+      "STEP": "ACT",
+      "DUE DATE": 2609,
+
+    },
+    {
+      "TASK NAME": "Jamie",
+      "LOB": "Kushnir",
+      "SUBTASKS": "Bell Electric Co",
+      "ASSIGNED TO": "3216 W Wabansia Ave",
+      "STEP": "ACT",
+      "DUE DATE": 2901,
+    
+    },
+    {
+      "TASK NAME": "Wenona",
+      "LOB": "Carmel",
+      "SUBTASKS": "Maier, Kristine M",
+      "ASSIGNED TO": "44 Bush St",
+      "STEP": "NSW",
+      "DUE DATE": 1220,
+   
+    },
+    {
+      "TASK NAME": "Deane",
+      "LOB": "Haag",
+      "SUBTASKS": "Malsbary Mfg Co",
+      "ASSIGNED TO": "9 Hamilton Blvd #299",
+      "STEP": "NSW",
+      "DUE DATE": 1235,
+
+    },
+    {
+      "TASK NAME": "Nenita",
+      "LOB": "Mckenna",
+      "SUBTASKS": "Southern Imperial Inc",
+      "ASSIGNED TO": "709 New Market St",
+      "STEP": "NSW",
+      "DUE DATE": 1455,
+
+    },
+    {
+      "TASK NAME": "Mayra",
+      "LOB": "Bena",
+      "SUBTASKS": "Buelt, David L Esq",
+      "ASSIGNED TO": "808 Glen Cove Ave",
+      "STEP": "NSW",
+      "DUE DATE": 1595,
+
+    },
+    {
+      "TASK NAME": "Samuel",
+      "LOB": "Arellanes",
+      "SUBTASKS": "Ryan, Barry M Esq",
+      "ASSIGNED TO": "286 Santa Rosa Ave",
+      "STEP": "NSW",
+      "DUE DATE": 1595,
+    
+    },
+    {
+      "TASK NAME": "Princess",
+      "LOB": "Saffo",
+      "SUBTASKS": "Asian Jewelry",
+      "ASSIGNED TO": "12398 Duluth St",
+      "STEP": "NSW",
+      "DUE DATE": 1835,
+      "city": "Auburn",
+     
+    },
+    {
+      "TASK NAME": "Michael",
+      "LOB": "Orehek",
+      "SUBTASKS": "Robinson, Michael C Esq",
+      "ASSIGNED TO": "892 Sw Broadway #8",
+      "STEP": "NSW",
+      "DUE DATE": 2000,
+
+    },
+    {
+      "TASK NAME": "Casandra",
+      "LOB": "Gordis",
+      "SUBTASKS": "Carlyle Abstract Co",
+      "ASSIGNED TO": "6 Walnut St",
+      "STEP": "NSW",
+      "DUE DATE": 2008,
+ 
+    },
+    {
+      "TASK NAME": "Leonor",
+      "LOB": "Prez",
+      "SUBTASKS": "Vinco Furniture Inc",
+      "ASSIGNED TO": "968 Delaware Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2017,
+
+    },
+    {
+      "TASK NAME": "Bernadine",
+      "LOB": "Elamin",
+      "SUBTASKS": "Tarix Printing",
+      "ASSIGNED TO": "61550 S Figueroa St",
+      "STEP": "NSW",
+      "DUE DATE": 2024,
+  
+    },
+    {
+      "TASK NAME": "Cherilyn",
+      "LOB": "Fraize",
+      "SUBTASKS": "Witchs Brew",
+      "ASSIGNED TO": "84826 Plaza Dr",
+      "STEP": "NSW",
+      "DUE DATE": 2030,
+
+    },
+    {
+      "TASK NAME": "Katie",
+      "LOB": "Magro",
+      "SUBTASKS": "Jones, Andrew D Esq",
+      "ASSIGNED TO": "8 E North Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2035,
+  
+    },
+    {
+      "TASK NAME": "Beckie",
+      "LOB": "Apodace",
+      "SUBTASKS": "Reich, Richard J Esq",
+      "ASSIGNED TO": "26 Ripley St #5444",
+      "STEP": "NSW",
+      "DUE DATE": 2068,
+
+    },
+    {
+      "TASK NAME": "Nickole",
+      "LOB": "Derenzis",
+      "SUBTASKS": "Lehigh Furn Divsn Lehigh",
+      "ASSIGNED TO": "2 Pompton Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2082,
+
+    },
+    {
+      "TASK NAME": "Fidelia",
+      "LOB": "Dampier",
+      "SUBTASKS": "Signs Now",
+      "ASSIGNED TO": "947 W Harrison St #640",
+      "STEP": "NSW",
+      "DUE DATE": 2083,
+
+    },
+    {
+      "TASK NAME": "Oren",
+      "LOB": "Lobosco",
+      "SUBTASKS": "Vei Inc",
+      "ASSIGNED TO": "1585 Salem Church Rd #59",
+      "STEP": "NSW",
+      "DUE DATE": 2083,
+
+    },
+    {
+      "TASK NAME": "Lou",
+      "LOB": "Kriner",
+      "SUBTASKS": "Joondeph, Jerome J Esq",
+      "ASSIGNED TO": "39 Broad St",
+      "STEP": "NSW",
+      "DUE DATE": 2092,
+
+    },
+    {
+      "TASK NAME": "Lachelle",
+      "LOB": "Andrzejewski",
+      "SUBTASKS": "Lucas Cntrl Systems Prod Deeco",
+      "ASSIGNED TO": "262 Montauk Blvd",
+      "STEP": "NSW",
+      "DUE DATE": 2126,
+    
+    },
+    {
+      "TASK NAME": "Tommy",
+      "LOB": "Gennusa",
+      "SUBTASKS": "Cooper And Raley",
+      "ASSIGNED TO": "2 New Brooklyn Rd",
+      "STEP": "NSW",
+      "DUE DATE": 2138,
+   
+    },
+    {
+      "TASK NAME": "Aide",
+      "LOB": "Ghera",
+      "SUBTASKS": "Nathaniel Electronics",
+      "ASSIGNED TO": "22 Livingston Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2138,
+ 
+    },
+    {
+      "TASK NAME": "Novella",
+      "LOB": "Fritch",
+      "SUBTASKS": "Voils, Otis V",
+      "ASSIGNED TO": "5 Ellestad Dr",
+      "STEP": "NSW",
+      "DUE DATE": 2145,
+    
+    },
+    {
+      "TASK NAME": "Sherman",
+      "LOB": "Mahmud",
+      "SUBTASKS": "Gencheff, Nelson E Do",
+      "ASSIGNED TO": "9 Memorial Pky Nw",
+      "STEP": "NSW",
+      "DUE DATE": 2150,
+  
+    },
+    {
+      "TASK NAME": "Madelyn",
+      "LOB": "Maestri",
+      "SUBTASKS": "Mervis Steel Co",
+      "ASSIGNED TO": "60 S 4th St",
+      "STEP": "NSW",
+      "DUE DATE": 2155,
+
+    },
+    {
+      "TASK NAME": "Ben",
+      "LOB": "Kellman",
+      "SUBTASKS": "Anderson, Julie A Esq",
+      "ASSIGNED TO": "30024 Whipple Ave Nw",
+      "STEP": "NSW",
+      "DUE DATE": 2159,
+
+    },
+    {
+      "TASK NAME": "Sharita",
+      "LOB": "Kruk",
+      "SUBTASKS": "Long, Robert B Jr",
+      "ASSIGNED TO": "8808 Northern Blvd",
+      "STEP": "NSW",
+      "DUE DATE": 2160,
+
+    },
+    {
+      "TASK NAME": "Maryrose",
+      "LOB": "Cove",
+      "SUBTASKS": "Brown Bear Bait Company",
+      "ASSIGNED TO": "1 Vogel Rd",
+      "STEP": "NSW",
+      "DUE DATE": 2166,
+  
+    },
+    {
+      "TASK NAME": "Johanna",
+      "LOB": "Saffer",
+      "SUBTASKS": "Springer Industrial Equip Inc",
+      "ASSIGNED TO": "750 Lancaster Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2194,
+   
+    },
+    {
+      "TASK NAME": "Kimberely",
+      "LOB": "Weyman",
+      "SUBTASKS": "Scientific Agrcltl Svc Inc",
+      "ASSIGNED TO": "7721 Harrison St",
+      "STEP": "NSW",
+      "DUE DATE": 2208,
+ 
+    },
+    {
+      "TASK NAME": "Claudia",
+      "LOB": "Gawrych",
+      "SUBTASKS": "Abe Goldstein Ofc Furn",
+      "ASSIGNED TO": "3 Wall St #26",
+      "STEP": "NSW",
+      "DUE DATE": 2229,
+
+    },
+    {
+      "TASK NAME": "German",
+      "LOB": "Dones",
+      "SUBTASKS": "Oaz Communications",
+      "ASSIGNED TO": "9 N Nevada Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2232,
+
+    },
+    {
+      "TASK NAME": "Rupert",
+      "LOB": "Hinkson",
+      "SUBTASKS": "Northwestern Mutual Life Ins",
+      "ASSIGNED TO": "1 E 17th St",
+      "STEP": "NSW",
+      "DUE DATE": 2250,
+
+    },
+    {
+      "TASK NAME": "Darnell",
+      "LOB": "Moothart",
+      "SUBTASKS": "Melco Embroidery Systems",
+      "ASSIGNED TO": "40 E 19th Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2257,
+
+    },
+    {
+      "TASK NAME": "Cordie",
+      "LOB": "Meikle",
+      "SUBTASKS": "Shapiro Bag Company",
+      "ASSIGNED TO": "40809 Rockburn Hill Rd",
+      "STEP": "NSW",
+      "DUE DATE": 2259,
+  
+    },
+    {
+      "TASK NAME": "Camellia",
+      "LOB": "Pylant",
+      "SUBTASKS": "Blackley, William J Pa",
+      "ASSIGNED TO": "570 W Pine St",
+      "STEP": "NSW",
+      "DUE DATE": 2259,
+   
+    },
+    {
+      "TASK NAME": "Jamal",
+      "LOB": "Korczynski",
+      "SUBTASKS": "Helricks Inc",
+      "ASSIGNED TO": "404 Broxton Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2261,
+    
+    },
+    {
+      "TASK NAME": "Holley",
+      "LOB": "Worland",
+      "SUBTASKS": "Lord Aeck & Sargent Architects",
+      "ASSIGNED TO": "2 Route 9",
+      "STEP": "NSW",
+      "DUE DATE": 2262,
+
+    },
+    {
+      "TASK NAME": "Julio",
+      "LOB": "Puccini",
+      "SUBTASKS": "Streator Onized Fed Crdt Un",
+      "ASSIGNED TO": "2244 Franquette Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2263,
+
+    },
+    {
+      "TASK NAME": "Lauran",
+      "LOB": "Tovmasyan",
+      "SUBTASKS": "United Christian Cmnty Crdt Un",
+      "ASSIGNED TO": "199 Maple Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2284,
+
+    },
+    {
+      "TASK NAME": "Jacklyn",
+      "LOB": "Wojnar",
+      "SUBTASKS": "Nationwide Insurance",
+      "ASSIGNED TO": "16949 Harristown Rd",
+      "STEP": "NSW",
+      "DUE DATE": 2287,
+  
+    },
+    {
+      "TASK NAME": "Norah",
+      "LOB": "Daleo",
+      "SUBTASKS": "Gateway Refrigeration",
+      "ASSIGNED TO": "754 Sammis Ave",
+      "STEP": "NSW",
+      "DUE DATE": 2289,
+   
+    },
+   
+  ];
+   
